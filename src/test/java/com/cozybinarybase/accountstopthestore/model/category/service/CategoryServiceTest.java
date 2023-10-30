ebff5d5b09668e2047751464797bf52420006431
9898s <sueh9898@icloud.com>
2023-10-31 02:50:33 +0900

    test: 카테고리 조회 테스트

--- src/test/java/com/cozybinarybase/accountstopthestore/model/category/service/CategoryServiceTest.java
+++ src/test/java/com/cozybinarybase/accountstopthestore/model/category/service/CategoryServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.cozybinarybase.accountstopthestore.model.category.dto.CategoryDeleteResponseDto;
+import com.cozybinarybase.accountstopthestore.model.category.dto.CategoryResponseDto;
 import com.cozybinarybase.accountstopthestore.model.category.dto.CategorySaveRequestDto;
 import com.cozybinarybase.accountstopthestore.model.category.dto.CategorySaveResponseDto;
 import com.cozybinarybase.accountstopthestore.model.category.dto.CategoryUpdateRequestDto;
@@ -14,6 +15,8 @@ import com.cozybinarybase.accountstopthestore.model.category.persist.repository.
 import com.cozybinarybase.accountstopthestore.model.member.persist.entity.MemberEntity;
 import com.cozybinarybase.accountstopthestore.model.member.persist.repository.MemberRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -122,5 +125,64 @@ class CategoryServiceTest {
 
     // then
     assertThat(categoryDeleteResponseDto.getCategoryId()).isEqualTo(1L);
+  }
+
+  @Test
+  void 카테고리_상세_조회_test() throws Exception {
+    // given
+    Long memberId = 1L;
+
+    // stub 1
+    MemberEntity hong = new MemberEntity();
+    hong.setId(1L);
+    hong.setName("홍길동");
+    hong.setEmail("test@test.com");
+    when(memberRepository.findById(any())).thenReturn(Optional.of(hong));
+
+    // stub 2
+    CategoryEntity existingCategory = new CategoryEntity(1L, "여행", hong);
+    when(categoryRepository.findById(any())).thenReturn(Optional.of(existingCategory));
+
+    // when
+    CategoryResponseDto categoryResponseDto =
+        categoryService.getCategory(1L, memberId);
+
+    String responseBody = objectMapper.writeValueAsString(categoryResponseDto);
+    System.out.println("테스트: " + responseBody);
+
+    // then
+    assertThat(categoryResponseDto.getCategoryName()).isEqualTo("여행");
+    assertThat(categoryResponseDto.getCategoryOwnerId()).isEqualTo(1L);
+  }
+
+  @Test
+  void 카테고리_리스트_조회_test() throws Exception {
+    // given
+    MemberEntity hong = new MemberEntity();
+    hong.setId(1L);
+    hong.setName("홍길동");
+    hong.setEmail("test@test.com");
+
+    CategoryEntity categoryEntity1 = new CategoryEntity(1L, "여행", hong);
+    CategoryEntity categoryEntity2 = new CategoryEntity(2L, "쇼핑", hong);
+
+    List<CategoryEntity> categoryEntityList = new ArrayList<>();
+    categoryEntityList.add(categoryEntity1);
+    categoryEntityList.add(categoryEntity2);
+
+    // stub 1
+    when(memberRepository.findById(any())).thenReturn(Optional.of(hong));
+
+    // stub 2
+    when(categoryRepository.findByMember_Id(any())).thenReturn(categoryEntityList);
+
+    // when
+    List<CategoryResponseDto> categoryResponseDtoList = categoryService.allCategory(1L);
+
+    String responseBody = objectMapper.writeValueAsString(categoryResponseDtoList);
+    System.out.println("테스트: " + responseBody);
+
+    // then
+    assertThat(categoryResponseDtoList.size()).isEqualTo(2);
   }
 }
