32750f662ebe3dded60aeeba958ee15bda52124d
9898s <sueh9898@icloud.com>
2023-11-01 03:17:10 +0900

    test: 자산 수정 테스트

--- src/test/java/com/cozybinarybase/accountstopthestore/model/asset/domain/AssetTest.java
+++ src/test/java/com/cozybinarybase/accountstopthestore/model/asset/domain/AssetTest.java
@@ -6,11 +6,14 @@ import static org.mockito.Mockito.when;
 
 import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetSaveRequestDto;
 import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetSaveResponseDto;
+import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetUpdateRequestDto;
+import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetUpdateResponseDto;
 import com.cozybinarybase.accountstopthestore.model.asset.dto.constants.AssetType;
 import com.cozybinarybase.accountstopthestore.model.asset.persist.entity.AssetEntity;
 import com.cozybinarybase.accountstopthestore.model.asset.persist.repository.AssetRepository;
 import com.cozybinarybase.accountstopthestore.model.member.persist.entity.MemberEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -34,7 +37,6 @@ class AssetTest {
   void 자산_추가_test() throws Exception {
     // given
     AssetSaveRequestDto assetSaveRequestDto = new AssetSaveRequestDto();
-    assetSaveRequestDto.setAssetName("월급");
 
     MemberEntity hong = new MemberEntity();
     hong.setId(1L);
@@ -45,7 +47,6 @@ class AssetTest {
     AssetEntity assetEntity = AssetEntity.builder()
         .id(1L)
         .type(AssetType.MONEY)
-        .name("현금")
         .member(hong)
         .build();
     when(assetRepository.save(any())).thenReturn(assetEntity);
@@ -59,6 +60,39 @@ class AssetTest {
     System.out.println("테스트: " + responseBody);
 
     // then
-    assertThat(assetSaveResponseDto.getAssetName()).isEqualTo("현금");
+    assertThat(assetSaveResponseDto.getAssetId()).isEqualTo(1L);
+  }
+
+  @Test
+  void 자산_수정_test() throws Exception {
+    // given
+    AssetUpdateRequestDto assetUpdateRequestDto = new AssetUpdateRequestDto();
+    assetUpdateRequestDto.setAssetType(AssetType.BANK);
+    assetUpdateRequestDto.setAssetName("월급");
+
+    MemberEntity hong = new MemberEntity();
+    hong.setId(1L);
+    hong.setName("홍길동");
+    hong.setEmail("test@test.com");
+
+    // stub 1
+    AssetEntity assetEntity = AssetEntity.builder()
+        .id(1L)
+        .type(AssetType.MONEY)
+        .name("현금")
+        .member(hong)
+        .build();
+    when(assetRepository.findById(any())).thenReturn(Optional.of(assetEntity));
+
+    // when
+    AssetUpdateResponseDto assetUpdateResponseDto = AssetUpdateResponseDto.fromEntity(
+        asset.update(1L, assetUpdateRequestDto)
+    );
+
+    String responseBody = objectMapper.writeValueAsString(assetUpdateResponseDto);
+    System.out.println("테스트: " + responseBody);
+
+    // then
+    assertThat(assetUpdateResponseDto.getAssetName()).isEqualTo("월급");
   }
 }
