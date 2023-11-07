645419b445145a12516df42ad7436ba8481e26b0
9898s <sueh9898@icloud.com>
2023-11-07 15:44:55 +0900

    test: 자산 수정 테스트

--- src/test/java/com/cozybinarybase/accountstopthestore/model/asset/service/AssetServiceTest.java
+++ src/test/java/com/cozybinarybase/accountstopthestore/model/asset/service/AssetServiceTest.java
@@ -7,6 +7,8 @@ import static org.mockito.Mockito.when;
 import com.cozybinarybase.accountstopthestore.model.asset.domain.Asset;
 import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetSaveRequestDto;
 import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetSaveResponseDto;
+import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetUpdateRequestDto;
+import com.cozybinarybase.accountstopthestore.model.asset.dto.AssetUpdateResponseDto;
 import com.cozybinarybase.accountstopthestore.model.asset.dto.constants.AssetType;
 import com.cozybinarybase.accountstopthestore.model.asset.persist.entity.AssetEntity;
 import com.cozybinarybase.accountstopthestore.model.asset.persist.repository.AssetRepository;
@@ -14,6 +16,8 @@ import com.cozybinarybase.accountstopthestore.model.member.domain.Member;
 import com.cozybinarybase.accountstopthestore.model.member.dto.constants.Authority;
 import com.cozybinarybase.accountstopthestore.model.member.persist.entity.MemberEntity;
 import com.cozybinarybase.accountstopthestore.model.member.service.MemberService;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -95,5 +99,63 @@ class AssetServiceTest {
     assertEquals(10, requestDto.getStatementDay());
     assertEquals(20, requestDto.getDueDay());
     assertEquals("메모", requestDto.getMemo());
+  }
+
+  @Test
+  void 자산_수정_test() throws Exception {
+    // given
+    AssetUpdateRequestDto requestDto = new AssetUpdateRequestDto();
+    requestDto.setAssetType(AssetType.CARD);
+    requestDto.setAssetName("우리카드");
+    requestDto.setAmount(100000L);
+    requestDto.setStatementDay(10);
+    requestDto.setDueDay(20);
+    requestDto.setMemo("메모 수정");
+    requestDto.setCreatedAt(LocalDateTime.of(2023, 11, 7, 11, 0, 0));
+    requestDto.setUpdatedAt(LocalDateTime.of(2023, 11, 7, 12, 0, 0));
+
+    MemberEntity member = new MemberEntity();
+    member.setId(1L);
+    member.setRole(Authority.USER);
+    member.setEmail("test@test.com");
+    member.setPassword("1234");
+    member.setName("홍길동");
+    Member loginMember = Member.fromEntity(member);
+
+    AssetEntity savedAsset = AssetEntity.builder()
+        .id(1L)
+        .type(AssetType.MONEY)
+        .name("월급")
+        .amount(200000L)
+        .statementDay(1)
+        .dueDay(2)
+        .memo("메모")
+        .member(member)
+        .build();
+
+    // stub 1
+    when(memberService.validateAndGetMember(loginMember)).thenReturn(member);
+
+    // stub 2
+    when(assetRepository.findById(any())).thenReturn(Optional.of(savedAsset));
+
+    // stub 3
+    when(assetRepository.existsByNameAndTypeAndMember_Id(any(), any(), any())).thenReturn(false);
+
+    // stub 4
+    when(assetRepository.save(any())).thenReturn(savedAsset);
+
+    // when
+    AssetUpdateResponseDto responseDto = assetService.updateAsset(1L, requestDto, loginMember);
+
+    // then
+    assertEquals("카드", responseDto.getAssetType());
+    assertEquals("우리카드", requestDto.getAssetName());
+    assertEquals(100000L, requestDto.getAmount());
+    assertEquals(10, requestDto.getStatementDay());
+    assertEquals(20, requestDto.getDueDay());
+    assertEquals("메모 수정", requestDto.getMemo());
+    assertEquals("2023-11-07T11:00", responseDto.getCreatedAt().toString());
+    assertEquals("2023-11-07T12:00", responseDto.getUpdatedAt().toString());
   }
 }
