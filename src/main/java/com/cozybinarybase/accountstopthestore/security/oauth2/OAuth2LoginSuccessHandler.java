400ebe73708589ee5284e5735d97ddeddcf41e1c
han <rudgksdl94@gmail.com>
2023-11-21 10:04:38 +0900

    oauth 로그인 로직 변경 밀 리다이렉트 설정

--- src/main/java/com/cozybinarybase/accountstopthestore/security/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/cozybinarybase/accountstopthestore/security/oauth2/OAuth2LoginSuccessHandler.java
@@ -1,5 +1,8 @@
 package com.cozybinarybase.accountstopthestore.security.oauth2;
 
+import com.cozybinarybase.accountstopthestore.model.member.dto.constants.AuthType;
+import com.cozybinarybase.accountstopthestore.model.member.persist.entity.MemberEntity;
+import com.cozybinarybase.accountstopthestore.model.member.persist.repository.MemberRepository;
 import com.cozybinarybase.accountstopthestore.security.TokenProvider;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -15,24 +18,29 @@ import org.springframework.stereotype.Component;
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
   private final TokenProvider tokenProvider;
+  private final MemberRepository memberRepository;
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
       Authentication authentication) throws IOException, ServletException {
     CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+    String email = (String) customOAuth2User.getAttributes().get("email");
+    MemberEntity member = memberRepository.findByEmail(email)
+        .orElseGet(() -> memberRepository.save(customOAuth2User.toEntity()));
+
+    if (member.getAuthType() == AuthType.EMAIL) {
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      response.getWriter().write("{\"message\": \"이메일 가입자입니다.\"}");
+      response.setStatus(HttpServletResponse.SC_OK);
+      //TODO: 클라이언트 쪽에서 처음 페이지 받기
+      response.sendRedirect("https://asts.cozybinarybase.com:8443/login");
+      return;
+    }
 
     String accessToken = this.tokenProvider.generateAccessToken(customOAuth2User);
     String refreshToken = this.tokenProvider.generateRefreshToken();
     this.tokenProvider.sendAccessAndRefreshToken(response, accessToken, refreshToken);
-
-    response.sendRedirect("http://localhost:5173");
+    response.sendRedirect("https://asts.cozybinarybase.com:8443/account");
   }
-
-//  private void loginSuccess(HttpServletResponse response, CustomOAuth2User customOAuth2User) throws IOException {
-//    String accessToken = tokenProvider.generateAccessToken(customOAuth2User);
-//    String refreshToken = tokenProvider.generateRefreshToken();
-//
-//    tokenProvider.sendAccessAndRefreshToken(response, accessToken, refreshToken);
-//    tokenProvider.updateRefreshToken(customOAuth2User.getEmail(), refreshToken);
-//  }
 }
